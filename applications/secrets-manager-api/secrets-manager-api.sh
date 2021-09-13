6b77b40e4b09b56f75ccf516170dfebf0af1d8d3
sseraj007 <sseraj007@gmail.com>
2021-09-13 13:53:13 +0530

    Update secrets-manager-api.sh

--- applications/secrets-manager-api/secrets-manager-api.sh
+++ applications/secrets-manager-api/secrets-manager-api.sh
@@ -4,15 +4,15 @@ echo '=============== Staring init script for Secrets Manager API ==============
 # save all env for debugging
 printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
-echo '==> Installing Node.js and NPM'
-sudo apt-get update
-sudo apt install curl -y
+echo '==> Installing Apache'
+yum install httpd
+systemctl start httpd
 
 echo '==> Extract api artifact to /var/secrets-manager-api'
 mkdir $ARTIFACTS_PATH/drop
-tar -xvf $ARTIFACTS_PATH/api.*.tar.gz -C $ARTIFACTS_PATH/drop/
+tar -xvf $ARTIFACTS_PATH/api.tar.gz -C $ARTIFACTS_PATH/drop/
 mkdir /var/secrets-manager-api/
-tar -xvf $ARTIFACTS_PATH/drop/drop/api.*.tar.gz -C /var/secrets-manager-api
+tar -xvf $ARTIFACTS_PATH/drop/drop/api.tar.gz -C /var/secrets-manager-api
 
 echo 'RELEASE_NUMBER='$RELEASE_NUMBER >> /etc/environment
 echo 'API_BUILD_NUMBER='$API_BUILD_NUMBER >> /etc/environment
